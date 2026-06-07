02193bf3b75603e813de2cbea7e971dc221d189f
José Milton de Moraes Silva Neto <jose.neto@nees.ufal.br>
2026-06-07 18:43:34 -0300

    feat: desenvolvendo financial

--- backend/ong_plus/urls.py
+++ backend/ong_plus/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('authentication.urls')),
+    path('api/v1/financial/', include('financial.urls')),
     path('api/', include('verification.urls')),
 ]
