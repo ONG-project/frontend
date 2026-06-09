72c72db7128a440b08ddae5da5e8bbe7c48b28e6
I'm just K <142046371+Kcodesufal@users.noreply.github.com>
2026-06-09 02:58:54 -0300

    Update settings.py

--- backend/ong_plus/settings.py
+++ backend/ong_plus/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS', 
-    default='http://localhost:5173,http://127.0.0.1:5173'
+    default='http://localhost:5173,http://127.0.0.1:5173,https://ongplus-1.onrender.com'
 ).split(',')
 
 ROOT_URLCONF = 'ong_plus.urls'
